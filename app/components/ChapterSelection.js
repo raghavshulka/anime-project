d9341936144720096e5f169fc94ea85c34177129
Himanshu shukla <himanshu4shukla4l@gmail.com>
2023-12-22 19:10:16 +0530

    ..

--- app/components/ChapterSelection.js
+++ app/components/ChapterSelection.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoIosArrowBack } from "react-icons/io";
@@ -16,10 +16,47 @@ const ChapterSelection = () => {
           </p>
         </div>
       </div>
-      <div className="pl-2">
-        <label className="relative inline-flex items-center mb-5 cursor-pointer">
-        </label>
+      <div className=" p-4 ">
+        <button
+          title="Toggle Theme"
+          class="
+        w-12 
+        h-6 
+        rounded-full 
+        p-1 
+        bg-gray-400 
+        dark:bg-gray-600 
+        relative 
+        transition-colors 
+        duration-500 
+        ease-in
+        focus:outline-none 
+        focus:ring-2 
+        focus:ring-blue-700 
+        dark:focus:ring-blue-600 
+        focus:border-transparent
+      "
+        >
+          <div
+            id="toggle"
+            class="
+            rounded-full 
+            w-4 
+            h-4 
+            bg-blue-600 
+            dark:bg-blue-500 
+            relative 
+            ml-0 
+            dark:ml-6 
+            pointer-events-none 
+            transition-all 
+            duration-300 
+            ease-out
+        "
+          ></div>
+        </button>
       </div>
+
       <div className="px-3 flex justify-between items-center">
         <div>
           <Popup
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,27 +1,65 @@
 import React from "react";
-import { FaInfinity } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
+import { FaInfinity, FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
     <div>
-      <div className=" w-full flex justify-between items-center px-5 py-6 ">
-        <div className=" font-normal  text-[15px] flex justify-center items-center gap-5">
-          <div className="   text-3xl">
-            <p>manga</p>
+      <div className="w-full bg-gray-800 text-white px-5 py-3 mb-5">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center justify-center gap-5">
+            <div className="text-3xl font-bold">
+              <p>manga.</p>
+            </div>
+            <p className="cursor-pointer hover:opacity-55   ease-in delay-200">
+              Search
+            </p>
+            <p className="cursor-pointer hover:opacity-55   ease-in delay-200">
+              Missing Manga
+            </p>
+            <p className="cursor-pointer hover:opacity-55   ease-in delay-200">
+              Recent Chapter
+            </p>
+            <p className="cursor-pointer hover:opacity-55   ease-in delay-200">
+              Contact
+            </p>
+            <p className="cursor-pointer hover:opacity-55   ease-in delay-200">
+              Anime Memes
+            </p>
+            <p className="cursor-pointer hover:opacity-55   ease-in delay-200">
+              New Mangas
+            </p>
           </div>
-          <p>Search</p>
-          <p>New Mangas</p>
-          <p>Recent Chapter</p>
-          <p>Missing Manga</p>
-          <p>Contact</p>
-          <p>Anime Memes</p>
-        </div>
 
-        <div className=" font-normal  text-[15px]  flex justify-center items-center gap-5">
-          <FaInfinity />
-          <FaSearch />
-          <button>switch</button>
+          <div className="flex items-center justify-center gap-5">
+            <FaInfinity size={25} />
+
+            <div class="relative flex  flex-col justify-center overflow-hidden ">
+              <div class="relative rounded-full text-black bg-slate-200  shadow-xl ring-1 ring-gray-900/5 ">
+                <div class="mx-auto max-w-md">
+                  <form action="" class="relative mx-auto w-max">
+                    <input
+                      type="search"
+                      class="peer cursor-pointer relative z-10 h-10 w-5 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-black "
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
